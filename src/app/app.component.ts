5820d9b28e6cf8af5550eb3f63306226a217891d
razibmahmud50 <razibmahmud50@gmail.com>
2020-05-11 00:31:54 +0600

    screenshot added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import {EnrollmentService} from"./enrollment.service"
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tdf';
+  topicHasError = true;
+  topics = ['Angular','React', 'Vue']
+  userModel = new User('Razib','razib@gmail.com', 9497777777, 'default', 'morning',true)
+
+  constructor(private enrollmentService: EnrollmentService){ }
+
+  topicValidate(value){
+    if(value==="default"){
+      this.topicHasError = true
+    }else{
+      this.topicHasError = false
+    }
+  }
+  onSubmit(){
+    this.enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data=> console.log("success", data),
+      error=> console.log('error', error)
+    )
+
+  }
 }
